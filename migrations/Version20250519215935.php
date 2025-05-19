b272919bd21cc921b1eb7e4d4b233e6ca0294f97
Mykolai Stepanenko <mykolai.stepanenko@appflame.com>
2025-05-20 01:00:51 +0300

    reset migrations

--- migrations/Version20250519215935.php
+++ migrations/Version20250519215935.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250509205113 extends AbstractMigration
+final class Version20250519215935 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,18 +20,16 @@ final class Version20250509205113 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE user_role');
-        $this->addSql('ALTER TABLE user ADD role_id INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, role_id INT DEFAULT NULL, email VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(255) DEFAULT NULL, INDEX IDX_8D93D649D60322AC (role_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649D60322AC FOREIGN KEY (role_id) REFERENCES role (id)');
-        $this->addSql('CREATE INDEX IDX_8D93D649D60322AC ON user (role_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user_role (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, role_id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649D60322AC');
-        $this->addSql('DROP INDEX IDX_8D93D649D60322AC ON user');
-        $this->addSql('ALTER TABLE user DROP role_id');
+        $this->addSql('DROP TABLE role');
+        $this->addSql('DROP TABLE user');
     }
 }
